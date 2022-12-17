49d26fad698106e1448039dd3693e5fae4a0bc8b
Halyson <92060880+HalysonSA@users.noreply.github.com>
2022-12-17 15:55:48 -0300

    mobile navbar collapse

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,12 @@
-import { AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai'
+import {
+    AiOutlineMenu,
+    AiOutlineShoppingCart,
+    AiOutlineUser,
+} from 'react-icons/ai'
 import { useDispatch } from 'react-redux'
 import { toggleSidebar } from '../../redux/SidebarSlice'
 import SearchBar from './SearchBar'
+
 const Navbar = () => {
     const dispatch = useDispatch()
 
@@ -11,14 +16,14 @@ const Navbar = () => {
 
     return (
         <nav className=" h-20 bg-gray-500 flex justify-center select-none text-white">
-            <div className="flex flex-row items-center font-Roboto justify-around w-9/12">
+            <div className="hidden md:flex   flex-row items-center font-Roboto justify-around w-9/12">
                 <span>
-                    <img src="/logo.png" alt="seu logo" className="h-20 w-20" />
+                    <img src="/logo.png" alt="seu logo" className="h-12 w-12" />
                 </span>
                 <span>
                     <SearchBar />
                 </span>
-                <span className="flex flex-row gap-5">
+                <span className="flex flex-row gap-5 ">
                     <div className="flex items-center flex-row cursor-pointer">
                         <AiOutlineUser size={25} /> Olá, visitante
                     </div>
@@ -32,7 +37,25 @@ const Navbar = () => {
                     </div>
                 </span>
             </div>
+            {/*Mobile Navbar*/}
+
+            <div className="flex flex-row justify-between items-center w-11/12 md:hidden">
+                <span>
+                    <img src="/logo.png" alt="seu logo" className="h-12 w-12" />
+                </span>
+                <span>
+                    <SearchBar />
+                </span>
+                <span>
+                    <button className="h-12 w-12 flex items-center justify-center">
+                        <AiOutlineMenu size={30} />
+                    </button>
+                </span>
+            </div>
         </nav>
     )
 }
 export default Navbar
+function useMediaQuery(arg0: string): [any] {
+    throw new Error('Function not implemented.')
+}
